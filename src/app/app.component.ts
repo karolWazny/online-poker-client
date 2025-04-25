2a2b5695a6685893c2753bd28b57255552432048
Karol Ważny <karol.wazny@orange.com>
2025-04-25 18:05:49 +0200

    lobby with initial css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
-import {MatDivider} from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar, MatIcon, MatIconButton, MatDivider],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatIconButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/lobby/lobby.component.ts
+++ src/app/pages/lobby/lobby.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import {PlayingCardWrapperComponent} from '../../playing-card-wrapper/playing-card-wrapper.component';
+import {MatFormField} from '@angular/material/form-field';
+import {MatButton} from '@angular/material/button';
+import {MatInput} from '@angular/material/input';
+import {MatLabel} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-lobby',
   imports: [
-    PlayingCardWrapperComponent
+    MatFormField,
+    MatButton,
+    MatInput,
+    MatLabel
   ],
   templateUrl: './lobby.component.html',
   styleUrl: './lobby.component.scss'
