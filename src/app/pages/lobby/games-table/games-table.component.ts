3a71787da8d09baf4443bc609b139c8c9149ac80
Karol Ważny <karol.wazny@orange.com>
2025-04-29 17:02:40 +0200

    added paginator

--- src/app/pages/lobby/games-table/games-table.component.ts
+++ src/app/pages/lobby/games-table/games-table.component.ts
@@ -1,26 +1,39 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import {MatButton} from '@angular/material/button';
+import {MatTable} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 import {GameInfo} from '../../../api/games/model/game-info';
 import {GamesTableDatasource} from './games-table.datasource';
-
 
 @Component({
   selector: 'app-games-table',
   imports: [
     MatTableModule,
-    MatButton
+    MatButton,
+    MatPaginator
   ],
   templateUrl: './games-table.component.html',
   styleUrl: './games-table.component.scss'
 })
-export class GamesTableComponent implements OnInit {
+export class GamesTableComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<GameInfo>;
 
   displayedColumns: string[] = ['id', 'name', 'players', 'blinds', 'buyIn', 'join'];
   dataSource!: GamesTableDatasource;
 
   ngOnInit(): void {
     this.dataSource = new GamesTableDatasource();
+  }
+
+  ngAfterViewInit(): void {
+    /*this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;*/
   }
 
 }
