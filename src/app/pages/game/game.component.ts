5756f5e9ad9584e1335eb5807282a7f3e06a0f9a
Karol Ważny <karol.wazny@orange.com>
2025-06-13 09:47:52 +0200

    inverted cards

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {AsyncPipe} from '@angular/common';
+import {PlayingCardWrapperComponent} from '../../playing-card-wrapper/playing-card-wrapper.component';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [
+    PlayingCardWrapperComponent
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
